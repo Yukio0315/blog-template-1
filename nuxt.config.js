ef3b7e9e0d5f7722da3e6c93dc339532f81afdec
Yukio Ueda <triangle.pillow@gmail.com>
2020-01-14 02:47:53 +0900

    added postCss

--- nuxt.config.js
+++ nuxt.config.js
@@ -90,6 +90,22 @@ export default {
           })
         )
       }
+    },
+    postcss: {
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      plugins: {
+        // Disable a plugin by passing false as value
+        'postcss-responsive-type': true,
+        'postcss-hexrgba': true,
+        'css-mqpacker': true
+      },
+      preset: {
+        // Change the postcss-preset-env settings
+        autoprefixer: {
+          grid: true
+        }
+      }
     }
   },
   generate: {
