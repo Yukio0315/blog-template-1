522ce411124a2d006b3bce227a383466c1650f82
Yukio Ueda <triangle.pillow@gmail.com>
2020-01-31 17:29:57 +0900

    made box component

--- components/TheHeroImage/TheHeroImage.vue
+++ components/TheHeroImage/TheHeroImage.vue
@@ -1,19 +1,10 @@
 <template src="./template.html" />
 
 <script>
+import TheTopInfoBox from '@/components/TheHeroImage/TheTopInfoBox/TheTopInfoBox.vue'
 export default {
-  computed: {
-    seenTopAnime() {
-      return this.$store.state.seenTopAnime
-    }
-  },
-  async mounted() {
-    if (!this.$store.state.seenTopAnime.showBox) {
-      await this.$delay(1500)
-      this.$store.commit('seenTopAnime', 'showBox')
-      await this.$delay(1000)
-      this.$store.commit('seenTopAnime', 'showTitle')
-    }
+  components: {
+    TheTopInfoBox
   }
 }
 </script>
---
+++ components/TheHeroImage/TheTopInfoBox/TheTopInfoBox.vue
@@ -0,0 +1,21 @@
+<template src="./template.html" />
+
+<script>
+export default {
+  computed: {
+    seenTopAnime() {
+      return this.$store.state.seenTopAnime
+    }
+  },
+  async mounted() {
+    if (!this.$store.state.seenTopAnime.showBox) {
+      await this.$delay(1500)
+      this.$store.commit('seenTopAnime', 'showBox')
+      await this.$delay(1000)
+      this.$store.commit('seenTopAnime', 'showTitle')
+    }
+  }
+}
+</script>
+
+<style src="./style.scss" lang="scss" scoped />
